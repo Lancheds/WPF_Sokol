2276a5d72d0b473ea49e26b1c3844605e5cb3433
Lancheds <57685771+Lancheds@users.noreply.github.com>
2022-12-12 01:14:16 +0300

    Add files via upload

---
+++ SQLiteApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Windows;
+namespace SQLiteApp
+{
+    public partial class MainWindow : Window
+    {
+        ApplicationContext db = new ApplicationContext();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        // при загрузке окна
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // гарантируем, что база данных создана
+            db.Database.EnsureCreated();
+            // загружаем данные из БД
+            db.Users.Load();
+            // и устанавливаем данные в качестве контекста
+            DataContext = db.Users.Local.ToObservableCollection();
+        }
+
+        // добавление
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            UserWindow UserWindow = new UserWindow(new User());
+            if (UserWindow.ShowDialog() == true)
+            {
+                User User = UserWindow.User;
+                db.Users.Add(User);
+                db.SaveChanges();
+            }
+        }
+    }
+}
